c374067a95179b4e397ae9b0cbf9aa346b645a35
Emily Lowing <elowing@gmail.com>
2015-02-28 00:12:52 -0500

    Parse mbox into csv!

--- app/parse.rb
+++ app/parse.rb
@@ -1,23 +1,57 @@
 require 'mbox'
+require 'csv'
 require 'pry'
 
-def sift(body)
-  chunks = body.split "\r\n\r\n"
-  invoiced_to = chunks[3]
-  email = chunks[4]
-  delivered_to = chunks[5]
-  [invoiced_to, email, delivered_to]
+def column_names
+  [ 'Date', 'Shopper Id', 'Product', 'Quantity', 'Price', 'Subtotal',
+    'Shipping', 'Tax', 'TOTAL', 'Inv Name', 'Inv Company', 'Inv Address',
+    'Inv City', 'Inv State', 'Inv Pst Code', 'Inv Country', 'Tel', 'Fax',
+    'Email' ]
 end
 
-def parse_mbox_file(mail)
-  body = mail.content.first.to_s
-  filtered_array = sift body
-  return filtered_array
+def parse(body)
+  relevant_info = []
+  content = body.split("\r\n").reject(&:empty?)
+  content.each_with_index do |line, i|
+    first_word = line.split(" ").first
+    case first_word
+    when 'Date'
+      relevant_info << line[14..-1]
+    when 'Product'
+      line = content[i+1]
+      product, quantity, price = line.split(":").map(&:strip)
+      relevant_info << product
+      relevant_info << quantity
+      relevant_info << price
+    else
+      next unless (column_names + ['Shopper', 'Inv']).include? first_word
+      relevant_info << line.split(":").map(&:strip)[1..-1]
+    end
+  end
+  relevant_info
 end
 
-Mbox.open('~/Downloads/Orders.mbox').each do |mail|
-  relevant_info = parse_mbox_file mail
-  File.open('tmp/test', 'w') do |f|
-    f.write relevant_info
+def filter(email)
+  body = email.content.first.to_s
+  parse body
+end
+
+def populate_csv
+  Mbox.open('~/Downloads/Orders.mbox').each_with_index do |email, i|
+    row = filter email
+    CSV.open('tmp/test.csv', 'a+') do |csv|
+      csv << row
+    end
   end
 end
+
+def seed_table_headers
+  CSV.open('tmp/test.csv', 'a+') { |csv| csv << column_names }
+end
+
+def main
+  seed_table_headers
+  populate_csv
+end
+
+main
